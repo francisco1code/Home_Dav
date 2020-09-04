00ef4ba839eb064f685b25fca13b389093222e6d
francisco Emanoel <Francisco170142329@gmail.com>
2020-09-04 19:39:52 -0300

    Cadastrando um novo usuario no banco de dados

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,11 +1,21 @@
+
+import User from '../models/User'
+
 class SessionController{
-    store(req,res){
-        res.json({
-            message: "Minha api"
-        });
+   async store(req,res){
+        const { email } = req.body;
+
+        let user = await User.findOne({ email});
+        if(!email){
+            user = await User.create( {email});
+        }
+
+        //let user = await User.create( {email})
+
+        res.json(user);
 
     }
 
 }
 
-export default SessionController();
+export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-
+import SessionController from './controllers/SessionController'
 const routes = new Router();
 
 
-routes.get('/', (req, res) => {
-    return res.json({ 
-        ok: true,
-        nome: "edson",
-        nome2: "xuxu"
-    })
-});
+routes.post('/sessions', SessionController.store);
 
 export default routes;
